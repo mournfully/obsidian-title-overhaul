c737b8296554ab3640c69b726ba09af5d76a4138
mournfully <98147018+mournfully@users.noreply.github.com>
2023-06-04 10:03:38 -0400

    clean up codebase
    merge testing and utils
    temporarily move excess code out of the way
    
    bf-temp
    
    update ts scope to ignore temp/

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,17 @@
 import { Platform } from "obsidian"
 import { settings } from './settings';
 
+export async function testCommand() {	
+    const file = app.workspace.getActiveFile()
+    console.log(file)
+    
+    let cache = app.metadataCache.getFileCache(file!) // <-- non-null assertion (!) operator
+    console.log(cache)
+
+    let cachedHeading = cache?.headings?.[0]?.heading 
+    console.log(cachedHeading)   
+}
+
 export type IndexedDocument = {
     path: string
     mtime: number
@@ -20,3 +31,4 @@ export function getExtension(path: string): string {
 export function isCacheEnabled(): boolean {
     return !Platform.isIosApp && settings.useDatabase
 }
+
--- temp/setExplorer.ts
+++ temp/setExplorer.ts
@@ -1,20 +1,6 @@
 import { cacheManager } from './cache'
 import { isFileIndexable } from './utils'
-import { TFileExplorerView, TFileExplorerItem } from 'obsidian'
-
-export async function setTabTitles(setHeading: boolean, setSingle: boolean, filePath: string | null): Promise<void> {    
-    let leaves = app.workspace.getLeavesOfType('markdown')
-    for (let leaf of leaves) {
-        if (!leaf) continue
-        if (setSingle && filePath != leaf?.view?.file?.path) continue
-        let file = await cacheManager.getFromLiveCache(leaf?.view?.file?.path)
-        let title = setHeading ? (file.heading ? file.heading : file.basename) : file.basename
-        leaf.tabHeaderInnerTitleEl.setText(title)
-        
-        // console.log(`setSingle: ${setSingle}, filePath: ${filePath}, viewPath: ${leaf?.view?.file?.path}, cachedHeading: ${file.heading}`)
-        // console.log(file)
-    }
-}
+import { TFileExplorerView, TFileExplorerItem, Events } from 'obsidian'
 
 export async function setExplorerItemTitles(state: boolean): Promise<void> {
     // https://effectivetypescript.com/2020/05/26/iterate-objects/
